9de3591e713be0ca52a0465e965ebd5575f0bfeb
slemanSafiah <54937541+slemanSafiah@users.noreply.github.com>
2020-07-29 04:55:22 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,7 @@ app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Headers', 'Origin , X-Requested-With, Content-Type, Accept , Authorization ');
     if (req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-        return res.status(201).json({})
+        return res.status(200).json({})
     }
     next();
 });
